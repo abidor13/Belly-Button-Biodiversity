ed36d3db3dce49483648e25e25a620c7593772b2
abidor13 <106851167+abidor13@users.noreply.github.com>
2022-09-05 13:24:19 -0600

    Update charts.js

--- charts.js
+++ charts.js
@@ -59,8 +59,8 @@ function buildCharts(sample) {
   // 2. Use d3.json to load and retrieve the samples.json file 
   d3.json("samples.json").then((data) => {
     // 3. Create a variable that holds the samples array. 
-    
-    var samples = data.sample;
+    // console.log(data);
+    var samples = data.samples;
 
     // 4. Create a variable that filters the samples for the object with the desired sample number.
     var sampleArray = samples.filter(sampleObj => sampleObj.id == sample);
@@ -87,7 +87,8 @@ function buildCharts(sample) {
         y: yticks,
         text: otu_labels.slice(0, 10).reverse(),
         type: "bar",
-        orientation: "h"
+        orientation: "h",
+          
       }
       
     ];
@@ -105,15 +106,15 @@ function buildCharts(sample) {
 
     // 1. Create the trace for the bubble chart.
     var bubbleData = [{
-      x: otu_Ids,
+      x: otu_ids,
       y: sample_values,
       text: otu_labels,
       type: "scatter",
       mode: "markers",
       marker: {
         size: sample_values,
-        color: otu_Ids,
-        colorscale: "Greys"
+        color: otu_ids,
+        colorscale: "Earth"
       }
     }];
 
@@ -172,7 +173,7 @@ function buildCharts(sample) {
     // 5. Create the layout for the gauge chart.
     var gaugeLayout = {
       title: "Belly Button Washing Frequency",
-      // bar: { color: "black" }
+      bar: { color: "black" }
     };
 
     // 6. Use Plotly to plot the gauge data and layout.
